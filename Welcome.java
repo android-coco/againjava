0272a9f34f6556e9daea32113649ac9040bd976e
yhlyl <yh_android@163.com>
2018-02-07 20:46:50 +0800

    modified Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -2,7 +2,7 @@
 * @Author: yhlyl
 * @Date:   2018-02-06 22:20:18
 * @Last Modified by:   yhlyl
-* @Last Modified time: 2018-02-06 23:06:38
+* @Last Modified time: 2018-02-07 00:29:32
 */
 public class Welcome 
 {
@@ -27,5 +27,19 @@ public class Welcome
 		float f = 0.1f;
 		double d = 1.0/10;
 		System.out.println(f == d);
+		//jdk7.0 二进制数
+		int a1 = 0b0000_0000_0000_0000_0000_0000_0000_0011;
+		int b1 = 1_3221_2222;
+		System.out.println(a1);
+		System.out.println(b1);
+
+		int c1 = 3;
+		int c2 = c1++;
+		int c3 = ++c1;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(2 >> 1);
+		System.out.println(2 << 1);
 	}
 }
